2583a759445ba15bcfc529993816e3aaefe7e8eb
Juan José Rodriguez <zoranbow@gmail.com>
2023-06-11 15:19:45 -0500

    init(Redux)

--- client/App.js
+++ client/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigation } from './src/routes/AppNavigation';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigation />
-      <StatusBar backgroundColor='black' style='auto' translucent={true} />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigation />
+        <StatusBar backgroundColor='black' style='auto' translucent={true} />
+      </NavigationContainer>
+    </Provider>
   );
 }
--- client/src/screens/cards/CardsScreen.jsx
+++ client/src/screens/cards/CardsScreen.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { View, Text } from 'react-native';
 import { styles } from './CardsScreen.styles';
 
 export const CardsScreen = () => {
+  const cards = useSelector((state) => state.cards);
+
+  useEffect(() => {
+    console.log('CARDS: ', cards.length);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={{ color: 'white' }}>CARDS</Text>
